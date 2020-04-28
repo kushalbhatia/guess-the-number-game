2d0262dce0112c1a0b9e2a8e20c8311b7f0b7a79
Kushal Bhatia <kushalb7@gmail.com>
2020-04-28 16:59:31 -0700

    f string

--- guess_the_num.py
+++ guess_the_num.py
@@ -2,9 +2,9 @@
 import random
 
 print('Hello. What is your name?')
-name = input() # 
+name = input() # user inputs their name
 
-print('Well, ' + name + ', I am thinking of a number between 1 and 20')
+print(f'{name.title()}, I am thinking of a number between 1 and 20')
 secretNumber = random.randint(1,20)
 
 for guessesTaken in range(1, 7): # since you only get 6 guesses (1-6)
@@ -19,7 +19,6 @@ for guessesTaken in range(1, 7): # since you only get 6 guesses (1-6)
         break # This condition is for the correct guess
 
 if guess == secretNumber:
-    print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses.')
-    # the variable guessesTaken is an int, so you need to add str() to concatenate to rest of string
+    print(f'Good job, {name}! You guessed my number in {str(guessesTaken)} guesses.') # the variable guessesTaken is an int, so you need to add str() to concatenate to rest of string
 else:
-    print('Nope. The number I was thinking of was ' + str(secretNumber))
+    print(f'Nope. The number I was thinking of was {str(secretNumber)}') 
