d9f358320a6f6b5f718f06c907fd05a7f084ccf6
Kushal Bhatia <kushalb7@gmail.com>
2020-04-28 17:10:01 -0700

    publish

--- guess_the_num.py
+++ guess_the_num.py
@@ -1,13 +1,13 @@
 # This is a guess the number game.
 import random
 
-print('Hello. What is your name?')
+print('Hello! What is your name?')
 name = input() # user inputs their name
 
 print(f'{name.title()}, I am thinking of a number between 1 and 20')
 secretNumber = random.randint(1,20)
 
-for guessesTaken in range(1, 7): # since you only get 6 guesses (1-6)
+for guessesTaken in range(1, 7): # since you only get 6 guesses (1-6), could also simply write range(6) as well
     print('Take a guess.')
     guess = int(input()) # user inputs a number, but input is always a string, so need to add int() in front
 
@@ -19,6 +19,6 @@ for guessesTaken in range(1, 7): # since you only get 6 guesses (1-6)
         break # This condition is for the correct guess
 
 if guess == secretNumber:
-    print(f'Good job, {name}! You guessed my number in {str(guessesTaken)} guesses.') # the variable guessesTaken is an int, so you need to add str() to concatenate to rest of string
+    print(f'Excellent, {name}! You guessed my number in {str(guessesTaken)} guesses.') # the variable guessesTaken is an int, so you need to add str() to concatenate to rest of string
 else:
-    print(f'Nope. The number I was thinking of was {str(secretNumber)}') 
+    print(f'Nope. The number I was thinking of was {str(secretNumber)}')
