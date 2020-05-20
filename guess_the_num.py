b1f074bb9a73ec359a696fe246e03aae6aecdb10
Kushal Bhatia <kushalb7@gmail.com>
2020-05-20 15:08:26 -0700

    updated

--- guess_the_num.py
+++ guess_the_num.py
@@ -9,7 +9,7 @@ secretNumber = random.randint(1,20)
 
 for guessesTaken in range(1, 7): # since you only get 6 guesses (1-6), could also simply write range(6) as well
     print('Take a guess.')
-    guess = int(input()) # user inputs a number, but input is always a string, so need to add int() in front
+    guess = int(input()) # user inputs a number, but input() is always a string, so need to add int() in front
 
     if guess < secretNumber:
       print('Your guess is too low.')
@@ -22,3 +22,4 @@ if guess == secretNumber:
     print(f'Excellent, {name}! You guessed my number in {str(guessesTaken)} guesses.') # the variable guessesTaken is an int, so you need to add str() to concatenate to rest of string
 else:
     print(f'Nope. The number I was thinking of was {str(secretNumber)}')
+    
